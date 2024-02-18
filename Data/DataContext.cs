102b5b94dc535ef0e569a03180856d0bb87dd8c7
TharushiJayawardena <tharushijayawardena03@gmail.com>
2024-02-18 10:23:36 +0530

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_API___ENtity_Framework.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
